e647d4f9f358642d6b1b857c81caa1a6a63194a0
Jason Horn <jhorn@redhat.com>
2020-08-12 09:48:34 -0400

    Modified tools for multiple env

--- tools/approve_csr.sh
+++ tools/approve_csr.sh
@@ -1,1 +1,22 @@
-oc get csr -o go-template='{{range .items}}{{if not .status}}{{.metadata.name}}{{"\n"}}{{end}}{{end}}' | xargs oc adm certificate approve
+#!/bin/bash
+
+function check {
+
+  # keep checking for CSRs until something 
+  until [ $(oc get csr -o go-template='{{range .items}}{{if not .status}}{{.metadata.name}}{{"\n"}}{{end}}{{end}}'|wc -l) -gt 1 ]
+  do
+    sleep 10
+  done
+  
+ echo "Found CSRs" 
+ approve
+}
+
+function approve {
+  echo "Approving CSRs"
+  oc get csr -o go-template='{{range .items}}{{if not .status}}{{.metadata.name}}{{"\n"}}{{end}}{{end}}' | xargs oc adm certificate approve
+  sleep 10
+}
+
+check
+check
